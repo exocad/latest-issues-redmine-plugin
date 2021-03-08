759604bc8544e94168fbaf533ab432bfca898df3
lmexo <lmeysel@exocad.com>
2021-03-08 11:29:11 +0100

    versioning migration

--- db/migrate/002_create_latest_issues_setups.rb
+++ db/migrate/002_create_latest_issues_setups.rb
@@ -1,4 +1,4 @@
-class CreateLatestIssuesSetups < ActiveRecord::Migration
+class CreateLatestIssuesSetups < ActiveRecord::Migration[5.2]
   def change
     create_table :latest_issues_setups do |t|
       t.integer :max_count
